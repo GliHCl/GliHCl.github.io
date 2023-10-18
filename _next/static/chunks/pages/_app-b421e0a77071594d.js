013477628625b979fe35ad657b9e86b6ac948547
toto04 <tommaso.morganti01@gmail.com>
2023-10-18 20:17:15 +0000

    Deploying to gh-pages from @ GolAndrea/hci-2023_gli-HCl@8bfede9ab94343c9c2fcbd36e848039598342fb3 🚀

---
+++ _next/static/chunks/pages/_app-b421e0a77071594d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{2955:function(_,n,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return e(7256)}])}},function(_){var __webpack_exec__=function(n){return _(_.s=n)};_.O(0,[774,179],function(){return __webpack_exec__(2955),__webpack_exec__(3419)}),_N_E=_.O()}]);
