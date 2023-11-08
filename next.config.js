4117b8605128468682958319ad8624afe5f62b75
Tommaso Morganti <tommaso.morganti01@gmail.com>
2023-11-08 12:14:43 +0100

    fix github.io assetprefix

--- next.config.js
+++ next.config.js
@@ -3,8 +3,11 @@ let basePath = ""
 
 if (process.env.GITHUB_ACTIONS) {
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "")
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
+  // check if the repo name doesn't end with .github.io
+  if (!/\.github\.io$/.test(repo)) {
+    assetPrefix = `/${repo}/`
+    basePath = `/${repo}`
+  }
 }
 
 /** @type {import('next').NextConfig} */
