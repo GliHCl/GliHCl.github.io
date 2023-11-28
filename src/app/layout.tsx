f7bce3b3ac710836b230cd31508e5d3e1689c3f3
Tommaso Morganti <tommaso.morganti@mail.polimi.it>
2023-11-28 02:17:47 +0100

    simple footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Inter, Poppins } from "next/font/google"
 import "./globals.css"
 import { Header } from "@/components/Header"
+import { Footer } from "@/components/Footer"
 
 const poppins = Poppins({
   weight: "700",
@@ -26,6 +27,7 @@ export default function RootLayout({
       <body className={inter.className + " " + poppins.variable}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   )
